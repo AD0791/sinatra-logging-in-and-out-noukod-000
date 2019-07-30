033a4166c3d6ee91b20a6f36c495a27a543c9e3b
Alexandro Disla <alexandrodisla@hotmail.com>
2019-07-30 18:17:50 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
 
     if @user
       session[:id] = @user.id
-      #binding.pry
+      binding.pry
       redirect to '/account'
     else
       erb :error
